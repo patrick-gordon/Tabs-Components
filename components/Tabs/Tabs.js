6bd38618051b5142d6f84f2ff1576578be319bcf
Patrick Gordon <patrickgordon42@yahoo.com>
2019-05-16 15:14:25 -0500

    fisnihed dropdown menu

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,10 +2,10 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+    this.element = element;
     
     // Get the custom data attribute on the Link
-    // this.data;
+    this.data = document.querySelector(`.tab-link[data-tab='${this.element.dataset.tab}']`);
     
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
@@ -19,7 +19,7 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links = document.querySelectorAll('.tabs-link')
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
